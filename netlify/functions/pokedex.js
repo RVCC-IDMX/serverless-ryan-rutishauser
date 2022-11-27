539ea998d109786653acfd21b71057e48b316199
ryan-rutishauser <98336166+ryan-rutishauser@users.noreply.github.com>
2022-11-27 00:09:02 -0500

    hoenn and kanto

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -3,14 +3,18 @@
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 // eslint-disable-next-line func-names
-exports.handler = async function () {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
+exports.handler = async function (event) {
+  const eventBody = JSON.parse(event.body);
+  // eslint-disable-next-line prefer-template
+  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region;
+
+  console.log(event);
 
   const response = await fetch(POKE_API);
   const data = await response.json();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify({ pokemon: data.pokemon_entries }),
   };
 };
